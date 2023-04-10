15b6970c5ceb0ce30e0acdb16d24495cc65e1920
Benjamin Debeerst <benjamin.debeerst@posteo.de>
2023-04-10 13:56:16 +0200

    View stuff in a table

--- plan.md
+++ plan.md
@@ -1,8 +1,18 @@
-* site mit eingabe: zeile
-* trennen bei komma
-* anzeige in tabelle
+x site mit eingabe: zeile
+x trennen bei komma
+x anzeige in tabelle
 
 * import profile:
   * define columns to import
+    * split char = ","
+    * always trim
+    * type = date | text | number
+    * col-no -> type
   * date and number parsing
 
+Flow
+  * Upload CSV
+  * Choose import profile
+  * Validate parsing is fine
+  * import
+  * categorize
--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,15 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, Attribute, div, input, text, ul, li, button)
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import List exposing (..)
 
 main = Browser.sandbox { init = init, update = update, view = view }
 
 type alias Model =
-  { content : List String
+  { content : List (List String)
   , newContent : String
   }
 
@@ -26,7 +27,7 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     Update ->
-      { model | content = model.content ++ [ model.newContent ] }
+      { model | content = model.content ++ [ model.newContent |> String.split "," |> List.map String.trim ] }
     Draft newContent -> { model | newContent = newContent }
 
 view : Model -> Html Msg
@@ -38,14 +39,10 @@ view model =
         input [ placeholder "Text to add", onInput Draft] []
       , button [ onClick Update ] [ text "Add" ]
       ]
-    , div [ id "ledger" ] [
-        ul [] ( model.content |> List.map viewRow)
-      ]
+    , div [ id "ledger" ] ( viewTable model.content )
     ]
   ]
 
-viewContent : List String -> List (Html Msg)
-viewContent l = List.map viewRow l
-
-viewRow : String -> Html Msg
-viewRow r = li [] [ text r ]
+viewTable table = table |> List.map (
+  \row -> tr [] ( row |> List.map ( \t -> td [] [ text t ]) )
+  )
