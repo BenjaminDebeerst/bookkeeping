cdcc7b464066a90f588a71246b4c86f80ad36e3f
Benjamin Debeerst <benjamin.debeerst@posteo.de>
2023-04-11 23:14:06 +0200

    split model into ui and data

--- plan.md
+++ plan.md
@@ -8,6 +8,7 @@ x anzeige in tabelle
     * always trim
     * type = date | text | number
     * col-no -> type
+    * combination columns: this or (-)that, this * that
   * date and number parsing
 
 Flow
@@ -16,3 +17,9 @@ Flow
   * Validate parsing is fine
   * import
   * categorize
+
+Arch
+  * Try a client side only implementation without server
+    * split elm state into UI state and data
+    * action to serialize, action to deserialize.
+    * using elm-serialize
--- src/Main.elm
+++ src/Main.elm
@@ -8,41 +8,56 @@ import List exposing (..)
 
 main = Browser.sandbox { init = init, update = update, view = view }
 
-type alias Model =
-  { content : List (List String)
-  , newContent : String
+type alias Data = List (List String)
+type alias UI =
+  { row: String
   }
 
-init : Model
-init =
-  { content = []
-  , newContent = ""
-  }
+type Model
+  = Init String
+  | Show Data UI
 
-type Msg =
-    Draft String
-  | Update
+init = Init ""
+
+type Msg
+  = SetFileName String
+  | Load
+  | Row String
+  | AddRow
+  | Save
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Update ->
-      { model | content = model.content ++ [ model.newContent |> String.split "," |> List.map String.trim ] }
-    Draft newContent -> { model | newContent = newContent }
+  case (model, msg) of
+    (Init _, SetFileName f) -> Init f
+    (Init f, Load) -> Show [[f,f,f]] (UI "")
+    (Show d ui, AddRow) -> Show (d ++ [ ui.row |> String.split "," |> List.map String.trim ]) ui
+    (Show d ui, Row r) -> Show d { ui | row = r }
+    (Show d ui, Save) -> model
+    (_, _) -> model
 
 view : Model -> Html Msg
 view model =
-  div [] [
-    div [ id "menu" ] [ text "Titel" ]
-  , div [ id "content" ] [
-      div [ id "edit" ] [
-        input [ placeholder "Text to add", onInput Draft] []
-      , button [ onClick Update ] [ text "Add" ]
+  case model of
+    Init s -> showInit
+    Show data ui ->
+      div [] [
+        div [ id "menu" ] [ text "Titel" ]
+      , div [ id "data" ] [
+          div [ id "edit" ] [
+            input [ placeholder "Text to add", onInput Row] []
+          , button [ onClick AddRow ] [ text "Add" ]
+          ]
+        , div [ id "ledger" ] ( viewTable data )
+        , button [ onClick Save ] [ text "Save" ]
+        ]
       ]
-    , div [ id "ledger" ] ( viewTable model.content )
-    ]
-  ]
 
 viewTable table = table |> List.map (
   \row -> tr [] ( row |> List.map ( \t -> td [] [ text t ]) )
   )
+
+showInit = div [] [
+             input [ placeholder "File", onInput SetFileName] []
+           , button [ onClick Load ] [ text "Load" ]
+           ]
