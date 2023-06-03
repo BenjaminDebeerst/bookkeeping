85b7923be31b84f5cf74fa6e170b1e2f17ac1f5d
Benjamin Debeerst <benjamin.debeerst@posteo.de>
2023-06-04 00:17:31 +0200

    Add account filter to Monthly

--- src/Pages/Monthly.elm
+++ src/Pages/Monthly.elm
@@ -1,12 +1,14 @@
 module Pages.Monthly exposing (Model, Msg, page)
 
 import Dict exposing (Dict)
-import Element exposing (IndexedColumn, el, indexedTable, shrink, spacing, text)
+import Element exposing (Element, IndexedColumn, column, el, indexedTable, paddingEach, paddingXY, shrink, spacing, text)
+import Element.Input as Input exposing (labelRight)
 import Gen.Params.Monthly exposing (Params)
 import Layout exposing (formatEuro, size, style)
 import Page
-import Persistence.Data exposing (Category, Data)
+import Persistence.Data exposing (Account, Category, Data)
 import Processing.Aggregation exposing (Aggregate, MonthAggregate, aggregate)
+import Processing.Filter as Filter exposing (any)
 import Processing.Model exposing (getEntries)
 import Processing.Ordering exposing (dateAsc)
 import Request
@@ -17,8 +19,8 @@ import View exposing (View)
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
 page shared req =
     Page.element
-        { init = init
-        , update = update
+        { init = init shared
+        , update = update shared
         , view = view shared
         , subscriptions = \_ -> Sub.none
         }
@@ -29,12 +31,13 @@ page shared req =
 
 
 type alias Model =
-    {}
+    { accountFilter : List Account
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+init : Data -> ( Model, Cmd Msg )
+init data =
+    ( { accountFilter = Dict.values data.accounts }, Cmd.none )
 
 
 
@@ -42,12 +45,26 @@ init =
 
 
 type Msg
-    = None
+    = FilterAddAccount Account
+    | FilterRemoveAccount Account
+    | FilterAllAccounts Bool
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+update : Data -> Msg -> Model -> ( Model, Cmd Msg )
+update data msg model =
+    case msg of
+        FilterAddAccount acc ->
+            ( { model | accountFilter = acc :: model.accountFilter }, Cmd.none )
+
+        FilterRemoveAccount acc ->
+            ( { model | accountFilter = List.filter (\a -> not <| a.id == acc.id) model.accountFilter }, Cmd.none )
+
+        FilterAllAccounts on ->
+            if on then
+                ( { model | accountFilter = Dict.values data.accounts }, Cmd.none )
+
+            else
+                ( { model | accountFilter = [] }, Cmd.none )
 
 
 
@@ -55,42 +72,72 @@ update msg model =
 
 
 view : Data -> Model -> View Msg
-view data _ =
+view data model =
     let
-        entries =
-            getEntries data [] dateAsc
+        filter =
+            [ model.accountFilter |> List.map Filter.filterAccount |> any ]
 
-        accounts =
-            Dict.values data.accounts
-
-        aggregatedData : List Aggregate
         aggregatedData =
-            accounts
-                |> List.map (aggregate entries)
+            aggregate <| getEntries data filter dateAsc
     in
     Layout.page "Monthly" <|
-        (aggregatedData
-            |> List.map
-                (\aggregate ->
-                    [ el style.h2 <| text <| aggregate.account.name
-                    , indexedTable [ spacing size.tiny ]
-                        { data = aggregate.rows
-                        , columns =
-                            [ { header = el style.header <| text "Month"
-                              , width = shrink
-                              , view = \i e -> el (style.row i) <| text e.month
-                              }
-                            , { header = el style.header <| text "Balance"
-                              , width = shrink
-                              , view = \i e -> el (style.row i) <| formatEuro [] e.balance
-                              }
-                            ]
-                                ++ categoryColumns data.categories
-                        }
-                    ]
-                )
-            |> List.concat
-        )
+        [ showFilters model data.accounts
+        , showData data aggregatedData
+        ]
+
+
+showFilters : Model -> Dict Int Account -> Element Msg
+showFilters model accounts =
+    column [ spacing size.s ]
+        [ el style.h2 <| text "Filters"
+        , Element.row []
+            ([ el [ paddingXY size.m 0 ] <| text "Accounts "
+             , Input.checkbox []
+                { onChange = FilterAllAccounts
+                , icon = Input.defaultCheckbox
+                , checked = List.length model.accountFilter == Dict.size accounts
+                , label = labelRight [ paddingEach { top = 0, right = size.l, bottom = 0, left = 0 } ] <| text <| "All"
+                }
+             ]
+                ++ List.map
+                    (\acc ->
+                        Input.checkbox []
+                            { onChange = filterAccount acc
+                            , icon = Input.defaultCheckbox
+                            , checked = List.member acc model.accountFilter
+                            , label = labelRight [ paddingEach { top = 0, right = size.l, bottom = 0, left = 0 } ] <| text <| acc.name
+                            }
+                    )
+                    (Dict.values accounts)
+            )
+        ]
+
+
+filterAccount : Account -> Bool -> Msg
+filterAccount acc add =
+    if add then
+        FilterAddAccount acc
+
+    else
+        FilterRemoveAccount acc
+
+
+showData : Data -> Aggregate -> Element msg
+showData data aggregate =
+    indexedTable [ spacing size.tiny ]
+        { data = aggregate.rows
+        , columns =
+            [ { header = el style.header <| text "Month"
+              , width = shrink
+              , view = \i e -> el (style.row i) <| text e.month
+              }
+            , { header = el style.header <| text "Balance"
+              , width = shrink
+              , view = \i e -> el (style.row i) <| formatEuro [] e.balance
+              }
+            ]
+                ++ categoryColumns data.categories
+        }
 
 
 categoryColumns : Dict Int Category -> List (IndexedColumn MonthAggregate msg)
