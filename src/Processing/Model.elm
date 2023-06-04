e80a2e34891a840323d037fd16a055e15d25bd4c
Benjamin Debeerst <benjamin.debeerst@posteo.de>
2023-06-04 23:12:29 +0200

    Store parsed results of CSVs

--- src/Processing/Model.elm
+++ src/Processing/Model.elm
@@ -4,7 +4,6 @@ import Dict exposing (Dict)
 import Maybe.Extra
 import Persistence.Data as Data exposing (Account, AccountStart, Category, Data, RawEntry)
 import Processing.BookEntry exposing (BookEntry, Categorization(..), EntrySplit)
-import Processing.CsvParser exposing (ParsedRow, parseCsvLine)
 import Processing.Filter exposing (Filter, all)
 import Processing.Ordering exposing (Ordering)
 import Result.Extra
@@ -19,46 +18,19 @@ getEntries data filters order =
 getEntriesAndErrors : Data -> List Filter -> Ordering BookEntry -> ( List BookEntry, List String )
 getEntriesAndErrors data filters order =
     Dict.values data.rawEntries
-        |> List.map (parseToEntry data)
-        |> List.map (Result.andThen (enrichRow data))
+        |> List.map (enrichRow data)
         |> Result.Extra.partition
         |> Tuple.mapFirst (List.filter (all filters))
         |> Tuple.mapFirst (List.sortWith order)
-
-
-type alias Entry =
-    { id : String
-    , row : Result String ParsedRow
-    , accountId : Int
-    , categorization : Maybe Data.Categorization
-    }
-
-
-parseToEntry : Data -> RawEntry -> Result String Entry
-parseToEntry data raw =
-    Dict.get raw.importProfile data.importProfiles
-        |> Maybe.map
-            (\profile ->
-                let
-                    row =
-                        parseCsvLine profile raw.line
-                in
-                Entry raw.id row raw.accountId raw.categorization
-            )
-        |> Result.fromMaybe "Import profile not found."
 
 
 andMap =
     Result.map2 (|>)
 
 
-enrichRow : Data -> Entry -> Result String BookEntry
+enrichRow : Data -> RawEntry -> Result String BookEntry
 enrichRow data entry =
-    Ok BookEntry
-        |> andMap (Ok entry.id)
-        |> andMap (Result.map .date entry.row)
-        |> andMap (Result.map .description entry.row)
-        |> andMap (Result.map .amount entry.row)
+    Ok (BookEntry entry.id entry.date entry.description entry.amount)
         |> andMap (Dict.get entry.accountId data.accounts |> Result.fromMaybe ("Account not found: " ++ String.fromInt entry.accountId))
         |> andMap (liftCategorization data entry.categorization |> Result.fromMaybe "Category not found")
 
