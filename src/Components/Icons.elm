757103caf6b288a00304534ea4936b180b3dfec9
Benjamin Debeerst <benjamin.debeerst@posteo.de>
2023-06-13 15:28:31 +0200

    Introduce duplicate detection on import

--- src/Components/Icons.elm
+++ src/Components/Icons.elm
@@ -1,7 +1,7 @@
-module Components.Icons exposing (checkMark, triangleDown, triangleUp, warnTriangle)
+module Components.Icons exposing (checkMark, copy, database, triangleDown, triangleUp, warnTriangle)
 
-import Element exposing (el)
-import FeatherIcons
+import Element exposing (Attribute, Element, el)
+import FeatherIcons exposing (Icon)
 import Svg.Attributes as Attributes
 
 
@@ -20,17 +20,25 @@ triangleDown attrs size =
     el [ Element.rotate <| degrees 180 ] <| triangleUp attrs size
 
 
-checkMark attrs size =
-    el attrs <|
-        Element.html
-            (FeatherIcons.check
-                |> FeatherIcons.toHtml [ Attributes.height <| String.fromInt size ]
-            )
+checkMark : List (Attribute msg) -> Int -> Element msg
+checkMark =
+    basicIcon FeatherIcons.check
 
 
-warnTriangle attrs size =
+warnTriangle : List (Attribute msg) -> Int -> Element msg
+warnTriangle =
+    basicIcon FeatherIcons.alertTriangle
+
+
+copy : List (Attribute msg) -> Int -> Element msg
+copy =
+    basicIcon FeatherIcons.copy
+
+
+basicIcon : Icon -> List (Attribute msg) -> Int -> Element msg
+basicIcon icon attrs size =
     el attrs <|
         Element.html
-            (FeatherIcons.alertTriangle
+            (icon
                 |> FeatherIcons.toHtml [ Attributes.height <| String.fromInt size ]
             )
--- src/Pages/ImportFile.elm
+++ src/Pages/ImportFile.elm
@@ -1,20 +1,23 @@
 module Pages.ImportFile exposing (Model, Msg, page)
 
-import Components.Layout as Layout exposing (formatDate, formatEuro, size, style, updateOrRedirectOnError, viewDataOnly)
+import Components.Icons exposing (copy, warnTriangle)
+import Components.Layout as Layout exposing (color, formatDate, formatEuro, size, style, tooltip, updateOrRedirectOnError, viewDataOnly)
 import Components.Table as T
 import Csv.Decode as Decode exposing (Error(..))
 import Csv.Parser as Parser exposing (Problem(..))
 import Dict exposing (Dict)
-import Element exposing (Attribute, Element, centerX, centerY, el, fill, height, indexedTable, paddingEach, paddingXY, text, width)
+import Element exposing (Attribute, Element, centerX, centerY, el, fill, height, indexedTable, onRight, padding, paddingEach, paddingXY, row, spacing, text, width)
 import Element.Border as Border
+import Element.Font as Font
 import Element.Input as Input
 import File exposing (File)
 import File.Select as Select
 import Gen.Params.ImportFile exposing (Params)
 import Html.Events exposing (preventDefaultOn)
 import Json.Decode as D
+import List.Extra
 import Page
-import Persistence.Data as Shared exposing (Account, Data, ImportProfile, RawEntry, rawEntry)
+import Persistence.Data as Shared exposing (Account, Data, ImportProfile, RawEntry, rawEntry, sha1)
 import Persistence.Storage as Storage
 import Processing.CsvParser as CsvParser exposing (ParsedRow)
 import Request
@@ -55,6 +58,7 @@ type Msg
     | GotFileName File
     | GotFile String String
     | ChooseImportProfile ImportProfile
+    | Abort
     | Store Account ImportProfile (List ParsedRow)
 
 
@@ -85,6 +89,9 @@ update data msg model =
 
                 _ ->
                     ( model, Cmd.none )
+
+        Abort ->
+            ( Pick, Cmd.none )
 
         Store account profile lines ->
             let
@@ -192,36 +199,156 @@ viewFileContents data profile filename content =
                 ++ parseFile data p content
 
 
+
+-- parsed CSV view
+
+
 parseFile : Data -> ImportProfile -> String -> List (Element Msg)
 parseFile data profile csvFileContent =
     case CsvParser.parse profile csvFileContent of
         Err error ->
             [ text "Could not parse the given file with the selected profile.", text (errorToString error) ]
 
-        Ok rows ->
-            let
-                n =
-                    List.length rows
-            in
-            if n == 0 then
-                []
+        Ok [] ->
+            [ text "The CSV file was empty. There's nothing to do." ]
 
-            else
-                [ text "Import to account: " ]
-                    ++ (Dict.values
-                            data.accounts
-                            |> List.map (\a -> Input.button style.button { onPress = Just (Store a profile rows), label = text a.name })
-                       )
-                    ++ [ text <| "Successfully parsed " ++ String.fromInt n ++ " rows:"
-                       , indexedTable T.tableStyle
-                            { data = rows
-                            , columns =
-                                [ T.textColumn "Date" (.date >> formatDate)
-                                , T.styledColumn "Amount" (.amount >> formatEuro)
-                                , T.textColumn "Description" .description
-                                ]
-                            }
-                       ]
+        Ok rows ->
+            viewParsedRows data profile rows
+
+
+viewParsedRows : Data -> ImportProfile -> List ParsedRow -> List (Element Msg)
+viewParsedRows data profile rows =
+    let
+        csvSize =
+            List.length rows |> String.fromInt
+
+        duplicates =
+            findDuplicateRows rows
+
+        ( annotatedRows, overlap ) =
+            annotateRows rows duplicates data.rawEntries
+
+        annotatedRowsToStore =
+            annotatedRows
+                |> List.filter (\ar -> not ar.alreadyImported)
+                |> List.Extra.uniqueBy (\ar -> sha1 ar.parsedRow.rawLine)
+
+        rowsToStore =
+            annotatedRowsToStore |> List.map .parsedRow
+
+        storeSize =
+            List.length rowsToStore
+    in
+    if storeSize == 0 then
+        [ text "Every row in this CSV has already been imported. There's nothing to do." ]
+
+    else
+        warning overlap (\n -> n ++ " of " ++ csvSize ++ " rows in this CSV have already been imported. They shall be skipped.")
+            ++ warning (Dict.size duplicates) (\_ -> "This CSV has " ++ csvSize ++ " rows, but only " ++ String.fromInt storeSize ++ " distinct duplicated rows. Duplicate rows will be imported only once. Are your rows sufficiently distinct?")
+            ++ [ text <| "Import to account: " ]
+            ++ [ row [ spacing size.s ]
+                    ([ Input.button style.button { onPress = Just Abort, label = text "Abort" } ]
+                        ++ (Dict.values
+                                data.accounts
+                                |> List.map (\a -> Input.button style.button { onPress = Just (Store a profile rowsToStore), label = text a.name })
+                           )
+                    )
+               ]
+            ++ [ text <| "The following " ++ String.fromInt storeSize ++ " rows shall be added:"
+               , indexedTable T.tableStyle
+                    { data = annotatedRowsToStore
+                    , columns =
+                        [ T.styledColumn "Info" <| annotation
+                        , T.textColumn "Date" (.parsedRow >> .date >> formatDate)
+                        , T.styledColumn "Amount" (.parsedRow >> .amount >> formatEuro)
+                        , T.textColumn "Description" (.parsedRow >> .description)
+                        ]
+                    }
+               ]
+
+
+warning : Int -> (String -> String) -> List (Element msg)
+warning number messageTemplate =
+    if number == 0 then
+        []
+
+    else
+        [ row []
+            [ warnTriangle [ padding size.xs, Font.color color.red ] size.l
+            , number |> String.fromInt |> messageTemplate |> text
+            ]
+        ]
+
+
+iconTooltip : (List (Attribute msg) -> Int -> Element msg) -> String -> Element msg
+iconTooltip icon hint =
+    icon [ Font.color color.black, tooltip onRight hint ] size.m
+
+
+annotation : AnnotatedRow -> Element msg
+annotation annotatedRow =
+    if annotatedRow.duplicates > 1 then
+        iconTooltip copy <|
+            "This row was found "
+                ++ String.fromInt annotatedRow.duplicates
+                ++ " times."
+
+    else
+        Element.none
+
+
+type alias AnnotatedRow =
+    { parsedRow : ParsedRow
+    , duplicates : Int
+    , alreadyImported : Bool
+    }
+
+
+annotateRows : List ParsedRow -> Dict String Int -> Dict String RawEntry -> ( List AnnotatedRow, Int )
+annotateRows parsedRows duplicates dbEntries =
+    parsedRows
+        |> List.foldr
+            (\row ( rows, n ) ->
+                let
+                    rowId =
+                        sha1 row.rawLine
+
+                    exists =
+                        Dict.member rowId dbEntries
+
+                    m =
+                        if exists then
+                            n + 1
+
+                        else
+                            n
+
+                    dups =
+                        Dict.get rowId duplicates |> Maybe.withDefault 1
+                in
+                ( AnnotatedRow row dups exists :: rows, m )
+            )
+            ( [], 0 )
+
+
+findDuplicateRows : List ParsedRow -> Dict String Int
+findDuplicateRows parsedRows =
+    parsedRows
+        |> List.foldl
+            (\row dict ->
+                Dict.update (sha1 row.rawLine)
+                    (\val ->
+                        case val of
+                            Just i ->
+                                Just (i + 1)
+
+                            Nothing ->
+                                Just 1
+                    )
+                    dict
+            )
+            Dict.empty
+        |> Dict.filter (\_ v -> v > 1)
 
 
 errorToString : Error -> String
@@ -244,11 +371,15 @@ errorToString error =
             other |> Decode.errorToString
 
 
+
+-- raw CSV (pre)view
+
+
 preview : Data -> Maybe ImportProfile -> String -> String -> List (Element Msg)
 preview data selectedProfile filename contents =
     case Parser.parse { fieldSeparator = ',' } contents of
         Ok csv ->
-            [ text <| "Loaded " ++ (csv |> List.length |> String.fromInt) ++ " rows from " ++ filename ++ ". This is how the raw data looks like:"
+            [ text <| "Loaded header and " ++ (csv |> List.length |> (\i -> i - 1) |> String.fromInt) ++ " rows from " ++ filename ++ ". This is how the raw data looks like:"
             , previewTable csv
             , Input.radioRow []
                 { onChange = ChooseImportProfile
