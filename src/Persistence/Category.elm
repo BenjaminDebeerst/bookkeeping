38ef398759ba555f0b451f1fe9723b570c132186
Benjamin Debeerst <benjamin.debeerst@posteo.de>
2023-11-11 15:06:23 +0100

    Add another test field to Category table

--- src/Persistence/Category.elm
+++ src/Persistence/Category.elm
@@ -9,7 +9,16 @@ type alias Categories =
 
 
 type alias Category =
-    CategoryV1
+    CategoryV2
+
+
+type alias CategoryV2 =
+    { id : Int
+    , name : String
+    , short : String
+    , long : String
+    , anotherField : Int
+    }
 
 
 type alias CategoryV1 =
@@ -22,7 +31,7 @@ type alias CategoryV1 =
 
 fromV0 : Dict Int CategoryV0 -> Categories
 fromV0 dict =
-    Dict.map (\_ -> v0v1) dict
+    Dict.map (\_ -> v0v1 >> v1v2) dict
 
 
 type alias CategoryV0 =
@@ -34,7 +43,7 @@ type alias CategoryV0 =
 
 category : Int -> String -> String -> Category
 category i s1 s2 =
-    CategoryV1 i s1 s2 ""
+    CategoryV2 i s1 s2 "" 0
 
 
 
@@ -44,6 +53,17 @@ category i s1 s2 =
 codec : S.Codec String Categories
 codec =
     S.dict S.int categoryCodec
+
+
+v2Codec : S.Codec String CategoryV2
+v2Codec =
+    S.record CategoryV2
+        |> S.field .id S.int
+        |> S.field .name S.string
+        |> S.field .short S.string
+        |> S.field .long S.string
+        |> S.field .anotherField S.int
+        |> S.finishRecord
 
 
 v1Codec : S.Codec String CategoryV1
@@ -72,34 +92,47 @@ v0Codec =
 type StorageVersions
     = V0 CategoryV0
     | V1 CategoryV1
+    | V2 CategoryV2
 
 
 categoryCodec : S.Codec String Category
 categoryCodec =
     S.customType
-        (\v0Encoder v1Encoder value ->
+        (\v0Encoder v1Encoder v2Encoder value ->
             case value of
                 V0 record ->
                     v0Encoder record
 
                 V1 record ->
                     v1Encoder record
+
+                V2 record ->
+                    v2Encoder record
         )
         |> S.variant1 V0 v0Codec
         |> S.variant1 V1 v1Codec
+        |> S.variant1 V2 v2Codec
         |> S.finishCustomType
         |> S.map
             (\value ->
                 case value of
                     V0 storage ->
-                        v0v1 storage
+                        (v0v1 >> v1v2) storage
 
                     V1 storage ->
+                        v1v2 storage
+
+                    V2 storage ->
                         storage
             )
-            V1
+            V2
 
 
 v0v1 : CategoryV0 -> CategoryV1
 v0v1 v0 =
     CategoryV1 v0.id v0.name v0.short ""
+
+
+v1v2 : CategoryV1 -> CategoryV2
+v1v2 v1 =
+    CategoryV2 v1.id v1.name v1.short v1.long 0
