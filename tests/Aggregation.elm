772946fc1236c1a2b1060aeb5f799d28ba149c8f
Benjamin Debeerst <benjamin.debeerst@posteo.de>
2024-03-01 21:49:47 +0100

    Rewrite/fix aggregation tests

--- tests/Aggregation.elm
+++ tests/Aggregation.elm
@@ -2,20 +2,21 @@ module Aggregation exposing (..)
 
 import Dict
 import Expect exposing (Expectation)
-import Persistence.Account exposing (Account, AccountStart)
-import Persistence.Category exposing (Category)
+import Persistence.Account exposing (Account, AccountStart, account)
+import Persistence.Category exposing (Category, CategoryGroup(..), category)
 import Processing.Aggregation exposing (MonthAggregate, aggregate)
+import Processing.Aggregator exposing (Aggregator)
 import Processing.BookEntry exposing (BookEntry, Categorization(..))
 import Test exposing (..)
 import Time.Date as Date
 
 
 cat1 =
-    Category 1 "Cat 1" "CAT1"
+    category 1 "Cat 1" "CAT1" Expense []
 
 
 cat2 =
-    Category 2 "Cat 2" "CAT2"
+    category 2 "Cat 2" "CAT2" Expense []
 
 
 categories : List Category
@@ -24,42 +25,61 @@ categories =
 
 
 account1 =
-    Account 1 "Account Name" <| AccountStart 10 2023 3
+    account 1 "Account One" 10 2023 3
 
 
 account2 =
-    Account 2 "Account Name 2" <| AccountStart 20 2023 4
+    account 2 "Account Name 2" 20 2023 4
 
 
 entries : List BookEntry
 entries =
-    [ BookEntry "a" (Date.date 2023 3 1) "" 1 account1 (Single cat1)
-    , BookEntry "b" (Date.date 2023 3 2) "" 1 account1 (Single cat2)
-    , BookEntry "c" (Date.date 2023 4 4) "" 5 account2 (Single cat1)
-    , BookEntry "d" (Date.date 2023 4 12) "" 1 account1 (Single cat2)
-    , BookEntry "e" (Date.date 2023 4 28) "" 1 account1 (Single cat2)
-    , BookEntry "f" (Date.date 2023 5 4) "" 1 account1 (Single cat1)
+    [ BookEntry "a" (Date.date 2023 3 1) "" 2 account1 (Single cat1)
+    , BookEntry "b" (Date.date 2023 3 2) "" 2 account1 (Single cat2)
+    , BookEntry "c" (Date.date 2023 4 4) "" 2 account2 (Single cat1)
+    , BookEntry "d" (Date.date 2023 4 6) "" 2 account1 (Single cat2)
+    , BookEntry "e" (Date.date 2023 4 9) "" 2 account1 (Single cat2)
+    , BookEntry "f" (Date.date 2023 5 4) "" 2 account1 (Single cat1)
     ]
 
 
 aggregate_book_entries : Test
 aggregate_book_entries =
-    describe "Entry aggregation"
-        [ test "aggregates by month for single account" <|
+    describe "Running an aggregate"
+        [ test "aggregates non-running sums, ignoring start values" <|
             \_ ->
-                (aggregate (List.filter (\be -> be.account.id == 1) entries)).rows
+                (aggregate
+                    (Date.date 2023 2 1)
+                    (Dict.fromList [ ( "Sum", 10 ) ])
+                    [ Aggregator "Sum" .amount False ]
+                    entries
+                ).rows
+                    -- Expecting here:
+                    -- - list starts at start date
+                    -- - sum is exactly sum of entries of list above for a given month
+                    -- - if there is no entry for a given month, the value is absent
+                    -- - list ends where the entries above end
                     |> Expect.equal
-                        [ { month = "March 2023", balance = 12, entries = Dict.fromList [ ( 1, 1 ), ( 2, 1 ) ] }
-                        , { month = "April 2023", balance = 14, entries = Dict.fromList [ ( 2, 2 ) ] }
-                        , { month = "May 2023", balance = 15, entries = Dict.fromList [ ( 1, 1 ) ] }
+                        [ { month = Date.date 2023 2 1, columns = Dict.empty }
+                        , { month = Date.date 2023 3 1, columns = Dict.fromList [ ( "Sum", 4 ) ] }
+                        , { month = Date.date 2023 4 1, columns = Dict.fromList [ ( "Sum", 6 ) ] }
+                        , { month = Date.date 2023 5 1, columns = Dict.fromList [ ( "Sum", 2 ) ] }
                         ]
-        , test "filters by month for multiple accounts" <|
+        , test " aggregates for running sums, using the start value" <|
             \_ ->
-                (aggregate entries).rows
+                (aggregate
+                    (Date.date 2023 2 1)
+                    (Dict.fromList [ ( "Cumulative", 10 ) ])
+                    [ Aggregator "Cumulative" .amount True ]
+                    entries
+                ).rows
+                    -- Expecting here:
+                    -- - Monthly sums are carried over to next month
+                    -- - starting sum is included in the sums
                     |> Expect.equal
-                        -- Yes, although account 2 starts only in April, its starting balance is added to March as well
-                        [ { month = "March 2023", balance = 32, entries = Dict.fromList [ ( 1, 1 ), ( 2, 1 ) ] }
-                        , { month = "April 2023", balance = 39, entries = Dict.fromList [ ( 1, 5 ), ( 2, 2 ) ] }
-                        , { month = "May 2023", balance = 40, entries = Dict.fromList [ ( 1, 1 ) ] }
+                        [ { month = Date.date 2023 2 1, columns = Dict.fromList [ ( "Cumulative", 10 ) ] }
+                        , { month = Date.date 2023 3 1, columns = Dict.fromList [ ( "Cumulative", 14 ) ] }
+                        , { month = Date.date 2023 4 1, columns = Dict.fromList [ ( "Cumulative", 20 ) ] }
+                        , { month = Date.date 2023 5 1, columns = Dict.fromList [ ( "Cumulative", 22 ) ] }
                         ]
         ]
